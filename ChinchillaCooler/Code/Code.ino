9ab78010e181299f06feadf15dcb01d933a2ad06
Henrique Azoubel <84883028+HenriqueAzo@users.noreply.github.com>
2024-03-23 00:21:21 -0300

    Update Code.ino
    code working for button and time sensitive activation (10:00-18:00)

--- ChinchillaCooler/Code/Code.ino
+++ ChinchillaCooler/Code/Code.ino
@@ -1,17 +1,61 @@
+#include <NTPClient.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+
+//dados para conexão na wi-fi
+const char *ssid     = "Deco2";
+const char *password = "Lolicasa8";
+
+//setando UTC -3 (-10800 segundos)
+const long utcOffsetInSeconds = -10800;
+int hora;//variavel para armazenar hora atual
+
+//variavel para armazenamento de contador de tempo e intervalo de checagem de horario
+unsigned long previousMillis = 0;
+unsigned long interval = 5000;
 
 const int botaoRele=5; //pino (D1) para colocar botao para ligar/desligar coolers. botao deve aterrar pino.
 const int controleRele=2; //pino (D4) de saída para controle dos relés
-bool sensorBotao; //variável de leitura do estado do botão
 
-void setup() {
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "south-america.pool.ntp.org", utcOffsetInSeconds);
+
+void setup(){
+  Serial.begin(115200);
+
+  //Setup dos pinos
   pinMode(botaoRele, INPUT_PULLUP);
   pinMode(controleRele, OUTPUT);
 
-  Serial.begin(115200);
+  //bloco para conexão com rede
+  WiFi.begin(ssid, password);
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
 }
 
 void loop() {
-  digitalWrite(controleRele, digitalRead(botaoRele));
+  unsigned long currentMillis = millis();//variavel para checar passagem de tempo
+  if(currentMillis - previousMillis >= interval){
+    //salvar o momento de ultima checagem
+    previousMillis = currentMillis;
+    timeClient.update();
+    hora=timeClient.getHours();
+    Serial.println(hora);
+    Serial.print(timeClient.getHours());
+    Serial.print(":");
+    Serial.print(timeClient.getMinutes());
+  }
+
+  if (digitalRead(botaoRele)==0){//checagem do botão
+    digitalWrite(controleRele, LOW);
+  }else if(hora>=10 && hora<=17){//checagem do horário: ligar relés entre 10:00 e 18:00.
+    digitalWrite(controleRele, LOW);
+  }else{//nada a fazer. relés desativados.
+    digitalWrite(controleRele, HIGH);
+  }
 }
